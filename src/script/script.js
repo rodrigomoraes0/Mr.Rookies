f6a964bb842865fd90dc22ea6bff35032bb761b5
rodrigomoraes0 <rodrigomoras97@gmail.com>
2025-02-17 21:29:52 +0000

    FINAL

--- src/script/script.js
+++ src/script/script.js
@@ -4,3 +4,8 @@ $(document).ready(function(){
         $('#mobile_btn').find('i').toggleClass('fa-x')
     });
 });
+
+$('#mobile_menu a').on('click', function(){
+    $('#mobile_menu').removeClass('active');
+    $('#mobile_btn').find('i').removeClass('fa-x');
+});
